43b5d0e8905c056eb253f2b52393189ba4027b4d
Kmkinnear <korykinnear@triad.rr.com>
2022-10-24 21:45:54 -0400

    update

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -38,7 +38,7 @@ SELECT DISTINCT ON (emp_no) e.emp_no,
 	de.from_date,
 	de.to_date,
 	ti.title
-INTO mentorship_eligibility
+--INTO mentorship_eligibility
 FROM employees AS e
 INNER JOIN dept_emp AS de
 	ON (e.emp_no = de.emp_no)
@@ -48,3 +48,32 @@ WHERE (de.to_date = '9999-01-01')
 	AND (birth_date BETWEEN '1965-01-01' AND '1965-12-31')
 	ORDER BY emp_no ASC;
 	
+--Find most recent job title for mentors
+SELECT COUNT(title), title
+INTO mentor_titles
+FROM mentorship_eligibility
+GROUP BY title
+ORDER BY count DESC
+
+SELECT * FROM mentor_titles
+
+--Create table for README
+SELECT e.emp_no,
+	e.first_name,
+	e.last_name,
+	ti.title,
+	ti.from_date,
+	ti.to_date
+INTO retirement_titles1
+FROM employees as e
+INNER JOIN titles as ti
+ON (e.emp_no = ti.emp_no)
+	WHERE (birth_date BETWEEN '1965-01-01' AND '1967-12-31')
+	ORDER BY e.emp_no ASC;
+	
+--Find most recent job title for mentors
+SELECT COUNT(title), title
+INTO mentor_titles1
+FROM retirement_titles1
+GROUP BY title
+ORDER BY count DESC
